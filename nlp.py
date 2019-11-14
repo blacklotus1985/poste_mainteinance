21824414078cde08ccfcf354802ffbfaf45f2110
Unknown <alexander.zeisberg85@gmail.com>
2019-11-14 17:32:46 +0100

    git push for databricks first commit

--- nlp.py
+++ nlp.py
@@ -35,7 +35,7 @@ telephone_channel_filter = filter_functions.telephone_channel_filter(df)
 df = pd.concat([self_channel_filter,telephone_channel_filter])
 df = df[df['tipo']!='R'].reset_index()
 df = df[['numero','description']]
-df = df.loc[0:5000,:]
+df = df.loc[0:conf.getint("PARAMETERS","desc_rows"),:]
 print("df shape before filter for lenght of descrpition = " +str(df.shape[0]))
 old_df = df.copy()
 
@@ -78,7 +78,7 @@ final_dict_list, data_frame_id_words = find_best_words(df=df,column_index_name='
 description_index_list = top_desciptions(cosine_sim)
 
 # loops all the description and gets indexes of all the descriptions that are within a threshold of similarity.
-threshhold_list,df_threshold = threshold_descriptions(df=df,matrix=cosine_sim,data_frame_id_words=data_frame_id_words,conf=conf,threshold=0.5,filename="threshold_poste_cluster_descriptions.csv",drop_duplicates=True)
+threshhold_list,df_threshold = threshold_descriptions(df=df,matrix=cosine_sim,data_frame_id_words=data_frame_id_words,conf=conf,threshold=0.5,filename="threshold_poste_cluster_descriptions_no_equals.csv",drop_duplicates=True)
 
 print(1)
 print(1)
